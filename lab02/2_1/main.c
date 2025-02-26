e1c2c52b746632dcc63f9637922d4b5c5711703c
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2025-02-26 12:54:01 +0100

    final lab2

---
+++ lab02/2_1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=2, b=3, c=5;
+    int wynik = (a+b) % c;
+    printf("%d\n", wynik);
+    return 0;
+}
---
+++ lab02/2_13/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a = 0.001;
+    printf("%f\n", 1+a);
+    printf("%f\n", exp(a));
+    return 0;
+}
