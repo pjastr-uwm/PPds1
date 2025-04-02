39196f1331854afdaa7d36e9a1187cb3406b0f54
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-02 12:55:07 +0200

    9_3

---
+++ lab12/9_3/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int check(int (*f1)(int), int (*f2)(int), int n){
+    if(n<=0){
+        exit(-1);
+    }
+    for(int i=0;i<=n;i++){
+        if (f1(i) != f2(i)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int foo1(int a){
+    return a;
+}
+
+int foo2(int a){
+    return a%5;
+}
+
+int main()
+{
+    printf("%d\n", check(foo1, foo2, 4));
+    printf("%d\n", check(foo1, foo2, 20));
+    return 0;
+}
