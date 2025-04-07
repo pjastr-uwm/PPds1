198adb8f1f316dda1aaf6c40e7f4494a7255e714
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-07 12:13:42 +0200

    10_1

---
+++ lab13/10_1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void findMaxValue(int numbers[], int size){
+    int temp = numbers[0];
+    for(int i=1;i<size;i++){
+        if (temp < numbers[i]){
+            temp = numbers[i];
+        }
+    }
+    printf("%d\n", temp);
+}
+
+void findMaxValue2(int * numbers, int size){
+    int temp = * numbers;
+    for(int i=1;i<size;i++){
+        if (temp < *(numbers+i)){
+            temp = *(numbers+i);
+        }
+    }
+    printf("%d\n", temp);
+}
+
+int main()
+{
+    int tab[] = {4,5,-9,8,0};
+    findMaxValue(tab, 5);
+    findMaxValue2(tab, 5);
+    return 0;
+}
