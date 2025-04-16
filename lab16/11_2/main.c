5a07b86192baf82639f71f8500b4133bbd446b44
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-16 12:15:17 +0200

    11_2

---
+++ lab16/11_2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countNums(char txt[]){
+    int counter=0;
+    for(int i=0;txt[i] !=0;i++){
+        if (48 <= txt[i] && txt[i]<= 57){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int main()
+{
+    printf("%d\n", countNums("abc123XYZ"));
+    return 0;
+}
