e09417e21dd4a81f87228e3c7cfd9686524552af
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-05-05 12:18:01 +0200

    12_3

--- lab18/12_1/main.c
+++ lab18/12_1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumMatrix(int n, int m, int tab[][m]){
+int sumMatrix(int n, int m, int tab[n][m]){
     int temp=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
