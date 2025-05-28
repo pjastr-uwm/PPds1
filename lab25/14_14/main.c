61577354eb6989f0404a0e5446ad92515da6af35
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-05-28 12:36:18 +0200

    14_14

---
+++ lab25/14_14/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element {
+    int x;
+    struct element * next;
+};
+
+int find(struct element * list, int a){
+    struct element * ptr = list->next;
+    while(ptr !=NULL){
+        if (ptr->x == a){
+            return 1;
+        }
+        ptr = ptr->next;
+    }
+    return 0;
+}
+
+int main()
+{
+    struct element * list = malloc(sizeof(struct element));
+    list->next = malloc(sizeof(struct element));
+    list->next->x = 3;
+    list->next->next = malloc(sizeof(struct element));
+    list->next->next->x = -4;
+    list->next->next->next = malloc(sizeof(struct element));
+    list->next->next->next->x = 5;
+    list->next->next->next->next = NULL;
+    printf("%d\n", find(list,12));
+    return 0;
+}
